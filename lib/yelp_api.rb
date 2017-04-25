8b3b2c002010299efcd52e1a8a9ec0f4317104c6
MartinCrane <martin.crane@gmail.com>
2017-04-25 17:38:11 -0400

    yelp api issues

--- lib/yelp_api.rb
+++ lib/yelp_api.rb
@@ -53,34 +53,6 @@ module YelpAPI
       "#{parsed['token_type']} #{parsed['access_token']}"
     end
 
-
-    # Make a request to the Fusion search endpoint. Full documentation is online at:
-    # https://www.yelp.com/developers/documentation/v3/business_search
-    #
-    # term - search term used to find businesses
-    # location - what geographic location the search should happen
-    #
-    # Examples
-    #
-    #   search("burrito", "san francisco")
-    #   # => {
-    #          "total": 1000000,
-    #          "businesses": [
-    #            "name": "El Farolito"
-    #            ...
-    #          ]
-    #        }
-    #
-    #   search("sea food", "Seattle")
-    #   # => {
-    #          "total": 1432,
-    #          "businesses": [
-    #            "name": "Taylor Shellfish Farms"
-    #            ...
-    #          ]
-    #        }
-    #
-    # Returns a parsed json object of the request
     def search(term, location)
       url = "#{API_HOST}#{SEARCH_PATH}"
       params = {
@@ -88,7 +60,6 @@ module YelpAPI
         location: location,
         limit: SEARCH_LIMIT
       }
-
       response = HTTP.auth(bearer_token).get(url, params: params)
       response.parse
     end
@@ -97,33 +68,15 @@ module YelpAPI
 
       url = "#{API_HOST}#{SEARCH_PATH}"
       params = token
-
       response = HTTP.auth(bearer_token).get(url, params: params)
       response.parse
     end
 
-    # Look up a business by a given business id. Full documentation is online at:
-    # https://www.yelp.com/developers/documentation/v3/business
-    #
-    # business_id - a string business id
-    #
-    # Examples
-    #
-    #   business("yelp-san-francisco")
-    #   # => {
-    #          "name": "Yelp",
-    #          "id": "yelp-san-francisco"
-    #          ...
-    #        }
-    #
-    # Returns a parsed json object of the request
     def business(business_id)
       url = "#{API_HOST}#{BUSINESS_PATH}#{business_id}"
-
       response = HTTP.auth(bearer_token).get(url)
       response.parse
     end
-
 
     options = {}
     OptionParser.new do |opts|
@@ -147,9 +100,7 @@ module YelpAPI
       end
     end.parse!
 
-
     command = ARGV
-
 
     case command.first
     when "search"
